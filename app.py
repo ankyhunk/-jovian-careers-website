5ef40a9da03142b4d6dbd6ddb12c19e28592f728
Ankur A A <59883315+ankyhunk@users.noreply.github.com>
2022-10-16 21:17:20 +0000

    Updated Files with secret key

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db,load_job_from_db,
-add_application_to_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_jovian():
-    jobs=load_jobs_from_db()  
-    return render_template('home.html', 
-                           jobs=jobs)
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
+
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs=load_jobs_from_db()
+  jobs = load_jobs_from_db()
   return jsonify(jobs)
+
 
 @app.route("/job/<id>")
 def show_job(id):
-  job=load_job_from_db(id)
+  job = load_job_from_db(id)
   if not job:
-    return "Not Found",404
-  return render_template('jobpage.html',job=job)
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
 
-@app.route("job/<id>/apply",methods=["post"])
+
+@app.route("/job/<id>/apply", methods=["post"])
 def apply_to_job(id):
-  data=request.form
-  job=load_job_from_db(id)
-  add_application_to_db(id,data)
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
   return render_template('application_submitted.html',
-                        application=data,
-                        job=job)
-  
+                         application=data,
+                         job=job)
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
